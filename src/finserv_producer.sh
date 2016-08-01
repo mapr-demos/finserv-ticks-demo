ce2d2c4fda18c7f41a6762b0eb6ca669e37ef5c9
Nick Amato <amaton@gmail.com>
2016-08-01 15:02:34 -0700

    delete duplicate

--- src/finserv_producer.sh
+++
@@ -1,1 +0,0 @@
-ls -1 ~/nyse/data/* | while read line; do java -cp `mapr classpath`:./nyse-taq-streaming-1.0-jar-with-dependencies.jar com.mapr.examples.Run producer data/$line /user/mapr/taq:trades; done
