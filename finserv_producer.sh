98aa5704d6c04c80218c90b845f065582ea766b6
Ian Downard <kayaking@gmail.com>
2016-08-11 12:22:19 -0700

    fix invalid classpath

--- finserv_producer.sh
+++ finserv_producer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export DATA=/home/mapr/test1/finserv-ticks-demo/data
-export STREAM=/mapr/tmclust1/user/mapr/taq
-export TOPIC=raw
-ls -1 $DATA | while read line; do java -cp `mapr classpath`:./target/nyse-taq-streaming-1.0-jar-with-dependencies.jar com.mapr.demo.finserv.Run producer $DATA/$line $STREAM:$TOPIC; done
+export STREAM=/user/mapr/taq
+export TOPIC=trades
+ls -1 $DATA | while read line; do java -cp `mapr classpath`:/home/mapr/nyse/nyse-taq-streaming-1.0-jar-with-dependencies.jar com.mapr.demo.finserv.Run producer $DATA/$line $STREAM:$TOPIC; done
