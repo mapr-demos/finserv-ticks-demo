9a2f5d5530f70fa1af9d70686ddb1c7ad7e7ef38
Ian Downard <kayaking@gmail.com>
2016-08-12 13:36:51 -0700

    allow directory to be specified as the command-line arg for source data

--- src/main/java/com/mapr/demo/finserv/Producer.java
+++ src/main/java/com/mapr/demo/finserv/Producer.java
@@ -12,79 +12,95 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.Properties;
 
 public class Producer {
 
-    public static KafkaProducer producer;
+    private static KafkaProducer producer;
+    private static LinkedList<File> datafiles = new LinkedList<>();
     static long records_processed = 0L;
+
 
     public static void main(String[] args) throws IOException {
         if (args.length < 3) {
             System.err.println("ERROR: You must specify the input data file and stream:topic.");
             System.err.println("USAGE:\n" +
-                    "\tjava -cp `mapr classpath`:./nyse-taq-streaming-1.0-jar-with-dependencies.jar com.mapr.examples.Run producer [source data file] [stream:topic]\n" +
+                    "\tjava -cp `mapr classpath`:./nyse-taq-streaming-1.0-jar-with-dependencies.jar com.mapr.examples.Run producer [source file | source directory] [stream:topic]\n" +
                     "Example:\n" +
                     "\tjava -cp `mapr classpath`:./nyse-taq-streaming-1.0-jar-with-dependencies.jar com.mapr.examples.Run producer data/taqtrade20131218 /usr/mapr/taq:trades");
 
         }
 
-        String topic =  args[2] ;
-        System.out.println("Publishing to topic: "+ topic);
+        String topic = args[2];
+        System.out.println("Publishing to topic: " + topic);
 
         configureProducer();
-        System.out.println("Opening file " + args[1]);
-        File f = new File(args[1]);
-        FileReader fr = new FileReader(f);
-        BufferedReader reader = new BufferedReader(fr);
-        String line = reader.readLine();
+        File directory = new File(args[1]);
 
-
-        try {
-            long startTime = System.nanoTime();
-            long last_update = 0;
-
-            while (line != null) {
-
-                long current_time = System.nanoTime();
-                String key = Long.toString(current_time);
-                ProducerRecord<String, byte[]> record = new ProducerRecord<>(topic, key, line.getBytes(Charsets.ISO_8859_1));
-
-                // Send the record to the producer client library.
-//                producer.send(rec);
-                producer.send(record,
-                        new Callback() {
-                            public void onCompletion(RecordMetadata metadata, Exception e) {
-                                records_processed++;
-                                if(e != null)
-                                    e.printStackTrace();
-                            }
-                        });
-
-
-                // Print performance stats once per second
-                if ((Math.floor(current_time - startTime)/1e9) > last_update)
-                {
-                    last_update ++;
-                    producer.flush();
-                    print_status(records_processed, 1, startTime);
+        if (directory.isDirectory()) {
+            for (final File fileEntry : directory.listFiles()) {
+                if (fileEntry.isDirectory()) {
+                    System.err.println("WARNING: skipping files in directory " + fileEntry.getName());
+                } else {
+                    datafiles.add(fileEntry);
                 }
-                line = reader.readLine();
             }
-
-        } catch (Exception e) {
-            System.err.println("ERROR: " + e);
-        } finally {
-            producer.flush();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Published " + records_processed + " messages to stream.");
-            System.out.println("Finished.");
-            producer.close();
+        } else {
+            datafiles.add(directory);
         }
+
+        System.out.println ("Publishing data from " + datafiles.size() + " files.");
+        long startTime = System.nanoTime();
+        long last_update = 0;
+
+        for (final File f : datafiles) {
+            FileReader fr = new FileReader(f);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+
+            try {
+                while (line != null) {
+                    long current_time = System.nanoTime();
+                    String key = Long.toString(current_time);
+                    ProducerRecord<String, byte[]> record = new ProducerRecord<>(topic, key, line.getBytes(Charsets.ISO_8859_1));
+
+                    // Send the record to the producer client library.
+//                producer.send(rec);
+                    producer.send(record,
+                            new Callback() {
+                                public void onCompletion(RecordMetadata metadata, Exception e) {
+                                    records_processed++;
+                                    if (e != null)
+                                        e.printStackTrace();
+                                }
+                            });
+
+
+                    // Print performance stats once per second
+                    if ((Math.floor(current_time - startTime) / 1e9) > last_update) {
+                        last_update++;
+                        producer.flush();
+                        print_status(records_processed, 1, startTime);
+                    }
+                    line = reader.readLine();
+                }
+
+            } catch (Exception e) {
+                System.err.println("ERROR: " + e);
+            }
+        }
+        producer.flush();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Published " + records_processed + " messages to stream.");
+        System.out.println("Finished.");
+        producer.close();
     }
 
     /* Set the value for a configuration parameter.
