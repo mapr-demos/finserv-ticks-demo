9463fba41ef7667bf2ada35480218bd0a0c757ce
Ian Downard <kayaking@gmail.com>
2016-09-08 13:51:18 -0700

    continuously consume all records in a specified topic and persist them in Hive for analysis in Zeppelin

--- src/main/java/com/mapr/demo/finserv/SparkQuerier.java
+++ src/main/java/com/mapr/demo/finserv/SparkQuerier.java
@@ -9,6 +9,10 @@ package com.mapr.demo.finserv;
  * EXAMPLE USAGE:
  * /opt/mapr/spark/spark-1.6.1/bin/spark-submit --class com.mapr.demo.finserv.SparkQuerier /mapr/tmclust1/user/mapr/nyse-taq-streaming-1.0-jar-with-dependencies.jar /user/mapr/taq:sender_1142
  *
+ * REFERENCES:
+ *   Spark Streaming API - http://spark.apache.org/docs/1.6.0/streaming-kafka-integration.html
+ *   Spark Streaming Guide - http://spark.apache.org/docs/1.6.0/streaming-programming-guide.html
+ *
  * AUTHOR:
  * Ian Downard, idownard@mapr.com
  *****************************************************************************/
@@ -25,13 +29,24 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.hive.*;
 import org.apache.spark.sql.*;
 import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.v09.KafkaUtils;
+import scala.Tuple2;
 
 import java.util.*;
 
 public class SparkQuerier {
+
+    private static final int NUM_THREADS = 1;
+
+    // Seconds at which streaming data will be divided into batches
+    private static final int BATCH_INTERVAL = 5000;
+
+    // Hive table name for persisted ticks
+    private static final String HIVE_TABLE = "streaming_ticks";
+
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("ERROR: You must specify the stream:topic.");
@@ -42,11 +57,10 @@ public class SparkQuerier {
         }
 
         SparkConf conf = new SparkConf()
-                .setAppName("Spark Streaming")
-                .setMaster("local[*]");
+                .setAppName("TAQ Spark Streaming")
+                .setMaster("local[" + NUM_THREADS + "]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        SQLContext sqlContext = new SQLContext(sc);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(2000));
+        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(BATCH_INTERVAL));
         HiveContext hiveContext = new org.apache.spark.sql.hive.HiveContext(sc.sc());
 
         String topic = args[0];
@@ -55,64 +69,66 @@ public class SparkQuerier {
         kafkaParams.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
         kafkaParams.put("value.deserializer","org.apache.kafka.common.serialization.ByteArrayDeserializer");
 
-        // Connect to Kafka topic
+        // Generate the schema for the dataframe that we will be constructing.
+        // NOTE, if you change this schema remember to remove the Hive table
+        // saved on disk or Spark will fail to update that table in foreachRDD.
+        List<StructField> fields = new ArrayList<StructField>();
+        fields.add(0,DataTypes.createStructField("date", DataTypes.TimestampType, true));
+        fields.add(1,DataTypes.createStructField("symbol", DataTypes.StringType, true));
+        fields.add(2,DataTypes.createStructField("price", DataTypes.DoubleType, true));
+        fields.add(3,DataTypes.createStructField("volume", DataTypes.DoubleType, true));
+        fields.add(4,DataTypes.createStructField("sender", DataTypes.StringType, true));
+        fields.add(DataTypes.createStructField("receivers",
+                DataTypes.createArrayType(DataTypes.StringType, true), true));
+
+        StructType schema = DataTypes.createStructType(fields);
+
+        // Create an input stream that directly pulls messages from Kafka Brokers without using any receiver.
+        // This will query Kafka for the latest offset in each topic+parition every BATCH_INTERVAL seconds.
         JavaPairInputDStream<String, byte[]> directKafkaStream = KafkaUtils.createDirectStream(ssc,
                 String.class, byte[].class, kafkaParams, topics);
 
-        System.out.println("Waiting for messages on topic " + topic);
-        // Build an RDD with the fields we want to query
+        System.out.println("Waiting for messages...");
+
+        // This foreachRDD is an output operator with at-least once semantics.
+        // In case of worker failure we may persist duplicate data to Hive.
+        // Reference: http://spark.apache.org/docs/latest/streaming-programming-guide.html#semantics-of-output-operations
         directKafkaStream.foreachRDD(rdd -> {
-
-            // Generate the schema for the dataframe that we will be constructing
-            List<StructField> fields = new ArrayList<StructField>();
-            fields.add(0,DataTypes.createStructField("date", DataTypes.StringType, true));
-            fields.add(1,DataTypes.createStructField("symbol", DataTypes.StringType, true));
-            fields.add(2,DataTypes.createStructField("price", DataTypes.DoubleType, true));
-            fields.add(3,DataTypes.createStructField("volume", DataTypes.DoubleType, true));
-            StructType schema = DataTypes.createStructType(fields);
-
             if (rdd.count() > 0) {
-                System.out.println("reading messages...");
-                // save the raw data string in each row
-                JavaRDD<String> tick = rdd.map(x -> new String(x._2));
+                System.out.println("RDD batch size = " + rdd.count());
                 // Parse each row using the helper methods in the Tick data structure
+                JavaRDD<String> tick = rdd.map(x -> new String(x._2));
                 JavaRDD<Row> rowRDD = tick.map(
                         new Function<String, Row>() {
                             public Row call(String record) throws Exception {
                                 if (record.trim().length() > 0) {
                                     Tick t = new Tick(record.trim());
-                                    return RowFactory.create(t.getDate(), t.getSymbolRoot(), t.getTradeVolume(), t.getTradePrice());
+                                    return RowFactory.create(new java.sql.Timestamp(t.getTimeInMillis()), t.getSymbolRoot(), t.getTradeVolume(), t.getTradePrice(), t.getSender(), t.getReceivers());
                                 } else {
-                                    return RowFactory.create("", "", "", "");
+                                    return RowFactory.create("", "", "", "", "", "");
                                 }
                             }
                         });
 
-                // Apply our user-defined schema to the RDD of parsed data
-                DataFrame tick_table = sqlContext.createDataFrame(rowRDD, schema);
-                // Create another dataframe to construct a Hive table and share with Zeppelin
-                DataFrame tick_table_for_zeppelin = hiveContext.createDataFrame(rowRDD, schema);
+                // Apply our user-defined schema to the RDD and export it to a Hive table
+                DataFrame batch_table = hiveContext.createDataFrame(rowRDD, schema);
+//                master_tick_table.unionAll(tick_table);
 
-                // Register the DataFrame as a table so we can query it with SQL
-                tick_table.registerTempTable("temptick");
+                batch_table.registerTempTable("batchTable");
+                hiveContext.sql("create table if not exists " + HIVE_TABLE + " as select * from batchTable");
+                hiveContext.sql("insert into " + HIVE_TABLE + " select * from batchTable");
 
+                // Print the schema that we will use for our Hive table
+//                tick_table_for_zeppelin.printSchema();
+                // Print the first 5 rows of our Hive table
+//                tick_table_for_zeppelin.show(5);
+                Row[] results = hiveContext.sql("SELECT count(*) from " + HIVE_TABLE).collect();
+                System.out.println("Hive table size = " + results[0].toString().replaceAll("\\[(.*?)\\]", "$1"));
+                // Save the Hive table so that it can be accessed by Zeppelin
+                // This will be saved in a path like, /mapr/ian.cluster.com/user/hive/warehouse/
+//                tick_table.write().mode(SaveMode.Append).saveAsTable("ticks");
+//                tick_table.saveAsTable("ticks", SaveMode.Append);
 
-                tick_table_for_zeppelin.printSchema();
-                tick_table_for_zeppelin.show(5);
-                tick_table_for_zeppelin.saveAsTable("tick", SaveMode.Append);
-
-
-                try {
-                    // Run a SQL query over the RDD that we registered as table.
-                    DataFrame sql_result = sqlContext.sql("SELECT symbol FROM temptick");
-                    JavaRDD javardd = sql_result.javaRDD();
-                    List<String> elements = javardd.map((Function<Row, String>) row ->
-                            row.getString(0)).collect();
-                    //elements.forEach(item -> System.out.println("Symbol: " + item));
-                    System.out.println("Loaded " + elements.size() + " rows from SQL query.");
-                } catch (Exception e) {
-                    System.out.println("ERROR: " + e.getMessage());
-                }
             }
         });
 
