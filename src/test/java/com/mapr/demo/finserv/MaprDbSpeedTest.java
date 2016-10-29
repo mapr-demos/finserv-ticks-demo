322d91374d2364006701e9f18f6e077607acbfe8
Ian Downard <kayaking@gmail.com>
2016-10-28 22:03:40 -0700

    make sends multithreaded

--- src/test/java/com/mapr/demo/finserv/MaprDbSpeedTest.java
+++ src/test/java/com/mapr/demo/finserv/MaprDbSpeedTest.java
@@ -45,7 +45,12 @@ public class MaprDbSpeedTest {
     @Parameterized.Parameters(name = "{index}: messageSize={0}, threads={1}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {10, 1}, {100, 1}, {500, 1}, {1000, 1}, {2000, 1}, {5000, 1}, {10000, 1}
+                {100, 1}, {1000, 1}, {10000, 1}, {100000, 1},
+                {100, 2}, {1000, 2}, {10000, 2}, {100000, 2},
+                {100, 4}, {1000, 4}, {10000, 4}, {100000, 4},
+                {100, 8}, {1000, 8}, {10000, 8}, {100000, 8},
+                {100, 16}, {1000, 16}, {10000, 16}, {100000, 16},
+                {100, 32}, {1000, 32}, {10000, 32}, {100000, 32}
         });
     }
 
@@ -53,7 +58,7 @@ public class MaprDbSpeedTest {
     private int messageSize;  // size of each message sent into Kafka
     private static final Document end = MapRDB.newDocument(new HashMap<String, Object>());
 
-
+    long records_saved = 0;
 //    private static final ProducerRecord<String, byte[]> end = new ProducerRecord<>("end", null);
 
     public MaprDbSpeedTest(int messageSize, int threadCount) {
@@ -61,35 +66,87 @@ public class MaprDbSpeedTest {
         this.threadCount = threadCount;
     }
 
+    private static class Sender extends Thread {
+        private final BlockingQueue<Document> queue;
+        private final Table table;
+        private int id;
+
+        private Sender(int id, Table table, BlockingQueue<Document> queue) {
+            this.queue = queue;
+            this.table = table;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            long count = 0;
+            try {
+                Document doc = queue.take();
+                while (doc != end) {
+                    table.insert(this.getName()+"-"+Long.toString(++count), doc);
+                    doc = queue.take();
+                }
+            } catch (InterruptedException e) {
+                System.out.printf("%s: Interrupted\n", this.getName());
+            }
+        }
+    }
+
     @Test
     public void testThreads() throws Exception {
         System.out.printf("messageSize = %d, threadCount = %d\n", messageSize, threadCount);
 
-        // Test with BUFFERWRITE disabled:
-        Boolean bufferwrite = false;
+        // Test with BUFFERWRITE enabled:
+        Boolean bufferwrite = true;
 
         // Create new table names.
         String tableName = "speedTest";
         // delete the old table if it's there
         if (MapRDB.tableExists(tableName)) {
-            System.out.println("deleting old table " + tableName);
             MapRDB.deleteTable(tableName);
         }
         // make a new table
         Table table = MapRDB.createTable(tableName);
         table.setOption(Table.TableOption.BUFFERWRITE, bufferwrite);
 
-        Tick tick = new Tick("080845201DAA                T 00000000400000088400N0000000070800014CT1001100710051006");
+        String tick_data = "080845201DAA                T 00000000400000088400N0000000070800014CT10011007100510061007";
+        char[] space = new char[messageSize - tick_data.length()];
+        Arrays.fill(space, '*');
+        Tick tick = new Tick(tick_data + new String(space));
         Document document = MapRDB.newDocument((Object) tick);
+
+        // Create a pool of sender threads.
+        ExecutorService pool = Executors.newFixedThreadPool(threadCount);
+
+        // We need some way to give each sender messages to publish.
+        // We'll do that via this list of queues.
+        List<BlockingQueue<Document>> queues = Lists.newArrayList();
+        for (int i = 0; i < threadCount; i++) {
+            // We use BlockingQueue to buffer messages for each sender.
+            // We use this type not for concurrency reasons (although it is thread safe) but
+            // rather because it provides an efficient way for senders to take messages if
+            // they're available and for us to generate those messages (see below).
+            BlockingQueue<Document> q = new ArrayBlockingQueue<>(1000);
+            queues.add(q);
+            // spawn each thread with a reference to "q", which we'll add messages to later.
+            pool.submit(new Sender(i, table, q));
+        }
 
         double t0 = System.nanoTime() * 1e-9;
         double batchStart = 0;
 
         // We'll break out of this loop when that timeout occurs.
-        long records_saved = 0;
         for (int i = 0; i >= 0 && i < Integer.MAX_VALUE; ) {
             for (int j = 0; j < BATCH_SIZE; j++) {
-                table.insert(Long.toString(++records_saved), document);
+                int qid = j % threadCount;
+                try {
+                    // Put a message to be published in the queue belonging to the sender we just selected.
+                    // That sender will automatically send this message as soon as possible.
+                    queues.get(qid).put(document);
+                } catch (Exception e) {
+                    // BlockingQueue might throw an IllegalStateException if the queue fills up.
+                    e.printStackTrace();
+                }
             }
             i += BATCH_SIZE;
             double t = System.nanoTime() * 1e-9 - t0;
@@ -102,47 +159,22 @@ public class MaprDbSpeedTest {
             // batch / dt = hundreds of messages sent per second for this batch
             data.printf("%d,%d,%d,%.3f,%.1f,%.3f,%.1f, %s\n", messageSize, threadCount, i, t, i / t, dt, BATCH_SIZE / dt, bufferwrite ? "true" : "false");
             if (t > TIMEOUT) {
-                System.out.println("Average tput = " + i/t);
+                System.out.println("\tbuffer = "+ bufferwrite +", messageSize = " + messageSize + ", Average tput = " + i/t);
                 break;
             }
         }
-
-
-
-        // Now test with BUFFERWRITE enabled:
-        bufferwrite = true;
+        for (int i = 0; i < threadCount; i++) {
+            queues.get(i).add(end);
+        }
+        pool.shutdown();
+        pool.awaitTermination(10, TimeUnit.SECONDS);
 
         if (MapRDB.tableExists(tableName)) {
-            System.out.println("deleting old table " + tableName);
             MapRDB.deleteTable(tableName);
         }
-        // make a new table
-        table = MapRDB.createTable(tableName);
-        table.setOption(Table.TableOption.BUFFERWRITE, bufferwrite);
 
-        t0 = System.nanoTime() * 1e-9;
-        batchStart = 0;
 
-        // We'll break out of this loop when that timeout occurs.
-        records_saved = 0;
-        for (int i = 0; i >= 0 && i < Integer.MAX_VALUE; ) {
-            for (int j = 0; j < BATCH_SIZE; j++) {
-                table.insert(Long.toString(++records_saved), document);
-            }
-            i += BATCH_SIZE;
-            double t = System.nanoTime() * 1e-9 - t0;
-            double dt = t - batchStart;
-            batchStart = t;
-            // i = number of messages sent)
-            // t = total elapsed time
-            // i/t = overall throughput (number of messages sent overall per second)
-            // dt = elapsed time for this batch
-            // batch / dt = hundreds of messages sent per second for this batch
-            data.printf("%d,%d,%d,%.3f,%.1f,%.3f,%.1f, %s\n", messageSize, threadCount, i, t, i / t, dt, BATCH_SIZE / dt, bufferwrite ? "true" : "false");
-            if (t > TIMEOUT) {
-                System.out.println("Average tput = " + i/t);
-                break;
-            }
-        }
+        // TODO test with BUFFERWRITE disabled:
+
     }
 }
